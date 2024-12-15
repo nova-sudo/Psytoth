6bebeb5b490dcf07c997c59e3d03ce1877344809
Abdelrahman Noaman <novaxaboud@gmail.com>
2024-12-16 01:07:00 +0200

    commit

--- backend/modals/journal.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const journalSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  content: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Journal', journalSchema);
--- backend/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { registerUser } = require('../controllers/authController'); // Import controller function
-const router = express.Router();
-
-router.post('/register', registerUser); // Call controller function
-
-module.exports = router;
